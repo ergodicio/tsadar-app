89bcca1e399e7a2fbdcacf3b25ea4483fef9c10b
archis <joglekara@users.noreply.github.com>
2025-04-24 10:34:32 -0700

    improved ux

--- tsadar_gui/tesseract_ui.py
+++ tsadar_gui/tesseract_ui.py
@@ -5,16 +5,15 @@ jax_config.update("jax_enable_x64", True)
 
 import numpy as np
 import os
-from matplotlib import pyplot as plt
-from numpy import ndarray
 
-# from scipy.optimize import minimize
+
 import tqdm, optax, equinox as eqx, yaml
 import plotly.graph_objects as go
 import streamlit as st
 
 from tsadar import ThomsonParams
 from tsadar.core.modules.ts_params import get_filter_spec
+
 from flatten_dict import flatten, unflatten
 
 
@@ -39,17 +38,22 @@ diff_wrt_to = ["ne", "Te", "amp1", "amp2", "lam"]
 jac_outputs = ["electron_spectrum"]
 
 
+def to_arr(x):
+    """Convert a float to a numpy array."""
+    return np.array(np.array(x, dtype=np.float64)).reshape(-1, 1)
+
+
 def to_numpy(x: dict[str, float]) -> np.ndarray:
     """Convert the parameter dictionary to a numpy array."""
     return np.concatenate([[x[k] for k in diff_wrt_to]])
 
 
-def to_dict(params: ndarray) -> dict:
+def to_dict(params: np.ndarray) -> dict:
     """Convert the numpy array to a parameter dictionary."""
     return {k: params[i] for i, k in enumerate(diff_wrt_to)}
 
 
-def mse(pred: ndarray, true: ndarray) -> float:
+def mse(pred: np.ndarray, true: np.ndarray) -> float:
     """Mean Squared Error."""
     mse = np.mean(np.square(pred - true))
     return mse
@@ -73,8 +77,22 @@ def grad_fn(parameters: np.ndarray, true_electron_spectrum: np.ndarray) -> np.nd
     return grad  # to_numpy(grad)
 
 
+def create_parameter_dict(_ts_params: ThomsonParams) -> dict:
+    """Create a dictionary of parameters from the ThomsonParams object."""
+    parameters = {
+        "ne": _ts_params.electron.normed_ne[0],
+        "Te": _ts_params.electron.normed_Te[0],
+        "amp1": _ts_params.general.normed_amp1[0],
+        "amp2": _ts_params.general.normed_amp2[0],
+        "lam": _ts_params.general.normed_lam[0],
+    }
+    return parameters
+
+
 def tesseract_ui():
     # Sample random true parameters
+    col1, col2 = st.columns(2)
+
     rng = np.random.default_rng()
     true_ne = rng.uniform(0.1, 0.7)
     true_Te = rng.uniform(0.5, 1.5)
@@ -89,22 +107,14 @@ def tesseract_ui():
     config["parameters"]["general"]["lam"]["val"] = true_lam
     true_ts_params = ThomsonParams(config["parameters"], num_params=1, batch=True, activate=True)
 
-    true_parameters = {
-        "ne": true_ts_params.electron.normed_ne[0],
-        "Te": true_ts_params.electron.normed_Te[0],
-        "amp1": true_ts_params.general.normed_amp1[0],
-        "amp2": true_ts_params.general.normed_amp2[0],
-        "lam": true_ts_params.general.normed_lam[0],
-    }
+    true_parameters = create_parameter_dict(true_ts_params)
     true_electron_spectrum = tsadaract.apply(true_parameters)["electron_spectrum"]
 
-    st.write("True parameters:")
-    st.json(true_parameters)
-    # st.write(f"ne: {true_ne:.2f}")
-    # st.write(f"Te: {true_Te:.2f}")
-    # st.write(f"amp1: {true_amp1:.2f}")
-    # st.write(f"amp2: {true_amp2:.2f}")
-    # st.write(f"lam: {true_lam:.2f}")
+    true_fitted_params, _ = true_ts_params.get_fitted_params(config["parameters"])
+
+    with col1:
+        st.write("True parameters:")
+        st.json(clean_dict(true_fitted_params))
 
     # create an initial guess for the parameters
     this_rng = np.random.default_rng()
@@ -121,25 +131,20 @@ def tesseract_ui():
     config["parameters"]["general"]["lam"]["val"] = init_lam
 
     fit_ts_params = ThomsonParams(config["parameters"], num_params=1, batch=True, activate=True)
-
-    fit_parameters = {
-        "ne": fit_ts_params.electron.normed_ne[0],
-        "Te": fit_ts_params.electron.normed_Te[0],
-        "amp1": fit_ts_params.general.normed_amp1[0],
-        "amp2": fit_ts_params.general.normed_amp2[0],
-        "lam": fit_ts_params.general.normed_lam[0],
-    }
-
+    fit_parameters = create_parameter_dict(fit_ts_params)
     parameters_np = to_numpy(fit_parameters)
 
     electron_spectrum = tsadaract.apply(fit_parameters)["electron_spectrum"]
 
     # plot true electron spectrum in a plotly chart in streamlit
+    with col2:
+        st.write("Fitted parameters:")
+        fit_param_holder = st.empty()
     fig_holder = st.empty()
+
     fig = go.Figure()
     fig.add_trace(go.Scatter(y=true_electron_spectrum, mode="lines+markers", name="True Electron Spectrum"))
     fig.add_trace(go.Scatter(y=electron_spectrum, mode="lines+markers", name="Fit Electron Spectrum"))
-    print(fig.data)
     fig.update_layout(title="Electron Spectrum", xaxis_title="Wavelength", yaxis_title="Amplitude")
     fig_holder.plotly_chart(fig)
 
@@ -147,11 +152,17 @@ def tesseract_ui():
     diff_params, static_params = eqx.partition(
         fit_ts_params, filter_spec=get_filter_spec(cfg_params=config["parameters"], ts_params=fit_ts_params)
     )
-
+    fit_parameters = create_parameter_dict(diff_params)
     opt_state = opt.init(fit_parameters)
 
+    updated_fitted_parameters = get_fitted_params_for_ui(fit_parameters, diff_params, static_params)
+    fit_param_holder.write("Estimated parameters:")
+    fit_param_holder.json(updated_fitted_parameters)
+
     if st.button("Fit"):
+
         for i in (pbar := tqdm.tqdm(range(1000))):
+
             parameters_np = to_numpy(fit_parameters)
             electron_spectrum = tsadaract.apply(fit_parameters)["electron_spectrum"]
             loss, grad_loss = mse(electron_spectrum, true_electron_spectrum), grad_fn(
@@ -162,9 +173,32 @@ def tesseract_ui():
             fit_parameters = eqx.apply_updates(fit_parameters, updates)
             pbar.set_description(f"Loss: {loss:.4f}")
 
-            if i % 1 == 0:
-                # fig.add_trace(go.Scatter(y=electron_spectrum, mode="lines+markers", name=f"Step {i+1}"))
-                fig.data[1].y = electron_spectrum
-                fig.data[1].name = f"Step {i+1}"
-                # fig.update_layout(title="Electron Spectrum", xaxis_title="Wavelength", yaxis_title="Value")
-                fig_holder.plotly_chart(fig)
+            fig.data[1].y = electron_spectrum
+            fig.data[1].name = f"Step {i+1}"
+            fig_holder.plotly_chart(fig)
+
+            fit_param_holder.write("Estimated parameters:")
+            updated_fitted_parameters = get_fitted_params_for_ui(fit_parameters, diff_params, static_params)
+            fit_param_holder.json(updated_fitted_parameters)
+
+
+def get_fitted_params_for_ui(fit_parameters, diff_params, static_params):
+    diff_params = eqx.tree_at(lambda x: x.electron.normed_ne, diff_params, to_arr(fit_parameters["ne"]))
+    diff_params = eqx.tree_at(lambda x: x.electron.normed_Te, diff_params, to_arr(fit_parameters["Te"]))
+    diff_params = eqx.tree_at(lambda x: x.general.normed_amp1, diff_params, to_arr(fit_parameters["amp1"]))
+    diff_params = eqx.tree_at(lambda x: x.general.normed_amp2, diff_params, to_arr(fit_parameters["amp2"]))
+    diff_params = eqx.tree_at(lambda x: x.general.normed_lam, diff_params, to_arr(fit_parameters["lam"]))
+    fit_ts_params = eqx.combine(diff_params, static_params)
+    updated_fitted_parameters, _ = fit_ts_params.get_fitted_params(config["parameters"])
+    # updated_fitted_parameters = {k: v[0] for k, v in updated_fitted_parameters.items() if k in diff_wrt_to}
+    updated_fitted_parameters = clean_dict(updated_fitted_parameters)
+    return updated_fitted_parameters
+
+
+def clean_dict(d: dict) -> dict:
+    for k, v in d.items():
+        if isinstance(v, dict):
+            d[k] = clean_dict(v)
+        else:
+            d[k] = round(float(np.squeeze(v)), 2)
+    return d
